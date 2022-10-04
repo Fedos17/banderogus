011a52eb52c1cb5a7d731f4c80395bf0edb90488
Fedos <dfkd81@gmail.com>
2022-10-04 15:44:19 +0300

    apdate form

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,5 @@
 const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
+const MODAL_ERROR_VALUE = 'input-error';
 
 const formModal = document.querySelector('#form-modal');
 const successModal = document.querySelector('#success-modal');
@@ -8,16 +9,23 @@ const openFormModalBtn = document.querySelector('#open-form-modal-btn');
 const launchBtn = document.querySelector('#launch-btn');
 const closeBtns = document.querySelectorAll('.close-tab');
 
+const userName = document.getElementById('user-name');
+const userEmail = document.getElementById('user-email');
+
 openFormModalBtn.addEventListener('click', () => {
     formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
 })
 
 const closeFormModal = () => {
     formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+    userName.value = '';
+    userEmail.value = '';
 };
 
 const closeSuccessModal = () => {
     successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+    userName.value = '';
+    userEmail.value = '';
 };
 
 const openSuccessModal = () => {
@@ -34,6 +42,12 @@ closeBtns.forEach(btn => {
 
 
 form.addEventListener('submit', e => {
+    // if(userName.value === '' || userEmail.value === ''){
+    //     userName.classList.add(MODAL_ERROR_VALUE);
+    //     userEmail.classList.add(MODAL_ERROR_VALUE);
+    // } else {
+    //     userName.classList.remove(MODAL_ERROR_VALUE);
+    //     userEmail.classList.remove(MODAL_ERROR_VALUE);
     e.preventDefault();
     const formData = new FormData(form);
 
@@ -48,5 +62,6 @@ form.addEventListener('submit', e => {
         setTimeout(closeSuccessModal, 3000);
       })
       .catch((error) => console.log('Sending form failed'));
+    // }
 })
 
